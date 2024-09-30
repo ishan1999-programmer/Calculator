a83da423d7b1da4c7d8389bba78eb17742b6d0a9
ishan1999-programmer <ishantripathi16@gmail.com>
2024-09-30 11:24:38 +0530

    Project Completed

--- app.js
+++ app.js
@@ -1,9 +1,95 @@
 let calci = document.querySelector(".calci");
 let result_screen = document.querySelector(".result_screen");
 let input_screen = document.querySelector(".input_screen");
-let buttons = document.querySelectorAll(".buttons_grid > :not(:last-child)");
+let buttons = document.querySelectorAll(".buttons_grid > *");
+let equals = document.querySelector("button[data-operator=equals]");
 let dark_light_switch = document.querySelector(".dark_light_switch");
 let circle = document.querySelector(".circle");
+
+buttons.forEach((button)=>{
+    button.addEventListener("click" , action);
+    button.addEventListener("mousedown" , ()=>{
+        button.classList.add("press");
+    });
+    button.addEventListener("mouseup" , ()=>{
+        button.classList.remove("press");
+    });
+});
+
+let result = "";
+function action(e){
+    if(e.target.dataset.operator=="all_clear"){
+        input_screen.innerText = "";
+        result_screen.innerText = "";
+        result = "";
+    }
+    else if(e.currentTarget.dataset.operator=="backspace"){
+        input_screen.innerText = input_screen.innerText.substring(0,input_screen.innerText.length-1);
+        result = result.substring(0,result.length-1);
+    }
+    else if(e.target.dataset.operator=="equals"){
+        let final_result = eval(result);
+        if(Number.isInteger(final_result)){
+            result_screen.innerText = String(final_result);
+        }
+        else{
+            result_screen.innerText = String(parseFloat(String(final_result.toFixed(3))));
+        }
+    }
+    else if(e.target.dataset.operator=="square_root"){
+        console.log("hiii");
+    }
+    else{
+        if(e.target.dataset.operator=="divide"){
+            if(result_screen.innerText == ""){
+                input_screen.innerText += e.target.innerText;
+                result += '/';
+            }
+            else{
+                input_screen.innerText = result_screen.innerText + e.target.innerText;
+                result = result_screen.innerText + '/';
+                result_screen.innerText = "";
+            }
+        }
+        else if(e.target.dataset.operator=="multiply"){
+            if(result_screen.innerText == ""){
+                input_screen.innerText += e.target.innerText;
+                result += '*';
+            }
+            else{
+                input_screen.innerText = result_screen.innerText + e.target.innerText;
+                result = result_screen.innerText + '*';
+                result_screen.innerText = "";
+            }
+        }
+        else if(e.target.dataset.operator=="plus"){
+            if(result_screen.innerText == ""){
+                input_screen.innerText += e.target.innerText;
+                result += '+';
+            }
+            else{
+                input_screen.innerText = result_screen.innerText + e.target.innerText;
+                result = result_screen.innerText + '+';
+                result_screen.innerText = "";
+            }
+        }
+         else if(e.target.dataset.operator=="minus"){
+            if(result_screen.innerText == ""){
+                input_screen.innerText += e.target.innerText;
+                result += '-';
+            }
+            else{
+                input_screen.innerText = result_screen.innerText + e.target.innerText;
+                result = result_screen.innerText + '-';
+                result_screen.innerText = "";
+            }
+        }
+        else{
+            input_screen.innerText += e.target.innerText;
+            result += e.target.innerText;
+        }
+    }
+}
 
 dark_light_switch.addEventListener("click" , change_mode);
 circle.addEventListener("click" , change_mode);
